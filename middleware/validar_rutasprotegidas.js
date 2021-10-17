5fce9529c41a5a54b284b78deecfd55e2f5e2109
arirome <arielarome15.com@gmail.com>
2021-10-17 18:05:08 -0300

    cambios

--- middleware/validar_rutasprotegidas.js
+++ middleware/validar_rutasprotegidas.js
@@ -3,12 +3,7 @@ const { response, request } = require('express')
 //verificar si tiene el rol de administrador
 const isadmin = (req = request, res = response, next) => {
 
-    //validacion para asegurarme que estoy llamando correctamente al admin
-    if (!req.usuario) {
-        return res.status(500).json({
-            msg: 'Se intenta verificar el role sin validar el token'
-        });
-    };
+   
 
     //destructurar 
     const { rol } = req.usuario;
@@ -29,12 +24,7 @@ const roluser = (...rol) => {
 //retornar una funcion que se ejecuta cuando se pase por el roluser
     return (req,res=response,next)=>{
 
-        //verificaciones
-        if (!req.usuario) {
-            return res.status(500).json({
-                msg: 'Se intenta verificar el role sin validar el token'
-            });
-        };
+    
 
         if (!rol.includes(req.usuario.rol)) {
 
